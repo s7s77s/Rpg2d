c7bfde23c058cffa2c4f5645b4556bbf9850f6ff
krononpark <57452349+krononpark@users.noreply.github.com>
2024-01-27 11:56:42 +0300

    ffff

--- loader.py
+++ loader.py
@@ -1,4 +1,7 @@
 import pygame
-
-topor_image = pygame.image.load('image/topor/1.png')
-player_image = pygame.image.load('image/player/top/1.png').convert_alpha()
+from script import *
+topor_image = pygame.image.load('image/topor/1.png').convert_alpha()
+player_image_top = load_image('image/player/top')
+player_image_bottom = load_image('image/player/bottom')
+player_image_left = load_image('image/player/left')
+player_image_right = load_image('image/player/right')
